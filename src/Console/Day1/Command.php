fed9eb8cfe66c9983b9d53cc3d5b9a99a3a3c539
Anhleidvig <anhleidvig@gmail.com>
2025-07-28 22:41:17 +0200

    day 1 solutions

--- src/Console/Day1/Command.php
+++ src/Console/Day1/Command.php
@@ -2,9 +2,9 @@
 
 namespace App\Console\Day1;
 
-use App\Core\Facades\App;
 use Closure;
 use Exception;
+use App\Core\Facades\App;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,64 +16,63 @@ use Symfony\Component\Console\Command\Command as SymfonyCommand;
 )]
 final class Command extends SymfonyCommand
 {
-    protected const int START_FLOOR = 0;
+    protected const int START_LEVEL = 0;
+    protected const int BASEMENT_LEVEL = -1;
     protected const string INSTRUCTION_LEVEL_UP = '(';
     protected const string INSTRUCTION_LEVEL_DOWN = ')';
 
     protected const int CHUNK_SIZE = 8000;
     protected const string INPUT_FILE_NAME = 'day-1-input.txt';
 
+    protected const string SOLUTION_1_MESSAGE = "The final level is: %d";
+    protected const string SOLUTION_2_MESSAGE = "The index of the first instruction leading to basement (%d) is: %d";
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $instructions = $this->readInputFile();
 
-        $this->partOneSolution($instructions);
-        $this->partTwoSolution($instructions);
+        $results = $this->solve($instructions, $output);
+
+        $output->writeln(sprintf(
+            static::SOLUTION_1_MESSAGE,
+            $results['finalLevel'],
+        ));
+        $output->writeln(sprintf(
+            static::SOLUTION_2_MESSAGE,
+            static::BASEMENT_LEVEL,
+            $results['basementIndex'],
+        ));
 
         return Command::SUCCESS;
     }
 
-    protected function partOneSolution(string $instructions): void
+    protected function solve(string $instructions, OutputInterface $output): array
     {
-        $finalLevel = $this->walkInstructions($instructions);
+        $basementIndex = null;
 
-        echo "The final floor is: $finalLevel" . PHP_EOL;
+        $finalLevel = static::walkInstructions(
+            function (int $currentLvl, int $index) use (&$basementIndex) {
+                if ($currentLvl === static::BASEMENT_LEVEL && is_null($basementIndex)) {
+                    $basementIndex = $index + 1;
+                }
+            },
+            $instructions
+        );
+
+        return compact('finalLevel', 'basementIndex');
     }
 
-    protected function partTwoSolution(string $instructions): void
+    protected static function walkInstructions(Closure $callback, string $instructions): int
     {
-        $currentLevel = 0;
-        $instructionIndex = false;
+        $currentLevel = static::START_LEVEL;
 
-        for ($i = static::START_FLOOR; $i < strlen($instructions); $i++) {
+        for ($i = static::START_LEVEL; $i < strlen($instructions); $i++) {
             $currentLevel += match ($instructions[$i]) {
                 static::INSTRUCTION_LEVEL_UP => +1,
                 static::INSTRUCTION_LEVEL_DOWN => -1,
             };
 
-            if ($currentLevel === -1) {
-                $instructionIndex = $i + 1;
-                break;
-            }
-        }
-
-        if (!$instructionIndex) {
-            echo "No index lead to -1 level" . PHP_EOL;
-            return;
-        }
-
-        echo "The index of the instruction that lead to -1 level is: $instructionIndex" . PHP_EOL;
-    }
-
-    protected function walkInstructions(string $instructions): int
-    {
-        $currentLevel = static::START_FLOOR;
-
-        for ($i = static::START_FLOOR; $i < strlen($instructions); $i++) {
-            $currentLevel += match ($instructions[$i]) {
-                static::INSTRUCTION_LEVEL_UP => +1,
-                static::INSTRUCTION_LEVEL_DOWN => -1,
-            };
+            $callback($currentLevel, $i);
         }
 
         return $currentLevel;
