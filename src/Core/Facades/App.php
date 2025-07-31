05e0613da876e8aa1a9dd2d647c5189e7ec9c225
Anhleidvig <anhleidvig@gmail.com>
2025-07-31 21:44:21 +0200

    add App facade API methods as doccomment

--- src/Core/Facades/App.php
+++ src/Core/Facades/App.php
@@ -2,6 +2,25 @@
 
 namespace App\Core\Facades;
 
+use App\Core\Application;
+use App\Core\Contracts\Application as ApplicationContract;
+use Symfony\Component\Console\Application as SymfonyApplication;
+
+/**
+ * @method ApplicationContract registerCommands(array $commands)
+ * @method ApplicationContract function make(string $basePath)
+ * @method SymfonyApplication getConsole()
+ * @method int handleCommand()
+ * @method string basePath(string $path = '')
+ * @method string bootstrapPath(string $path = '')
+ * @method string storagePath(string $path = '')
+ * @method void setHasbeenBootstrapped()
+ * @method bool hasBeenBootstrapped()
+ * @method string getName()
+ * @method string getVersion()
+ *
+ * @see Application
+ */
 class App extends Facade
 {
     public static function accessor(): string
