32576fc150026a6dea116dcb1bccc0aa2bb340a5
Anhleidvig <anhleidvig@gmail.com>
2025-08-01 22:07:06 +0200

    feat: Register the command in the app, so it can be ran

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,11 @@
 
 use App\Core\Application;
 use App\Console\Day1\Day1Command;
+use App\Console\Day2\Day2Command;
 use App\Console\Day1\InstructionSolver;
 
 return Application::make(basePath: dirname(__DIR__))
     ->registerCommands([
         new Day1Command(new InstructionSolver()),
+        new Day2Command(),
     ]);
